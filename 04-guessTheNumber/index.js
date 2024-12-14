1e8534f68dba83849265d1b9c18d1582b36bbb41
Ankit Bhatt <ankit92b@gmail.com>
2024-12-15 00:31:05 +0530

    Add more game logic and end game functionality

--- 04-guessTheNumber/index.js
+++ 04-guessTheNumber/index.js
@@ -14,37 +14,78 @@ const guesses = document.querySelector('#guesses');
 const showChances = document.querySelector('#chances');
 const lowHigh = document.querySelector('#lowHigh');
 
-let canPlay = false;
-let prevGuess = [];
-let attempt = 10;
 
+let prevGuess = [];
+let attempts = 10;
+let msg = '';
 
 console.log(randoNum);
 
-guesses.innerHTML = 'None';
-showChances.innerHTML = attempt;
-lowHigh.innerHTML = 'Start Guessing'
-
+update('Start Guessing');
+userInput.focus();
 
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  const guess = parseInt(userInput.value)
-  if (isValid) {
-    checkGuess(guess)
+  const guess = parseInt(userInput.value);
+
+  if (isValid(guess)) {
+    checkGuess(guess);
+  } else {
+    customAlert(msg);
   }
 })
 
+endBtn.addEventListener('click', endGame)
 
 function isValid(guess) {
-  return !isNaN(guess) && guess > 0 && guess <= 100;
+  if (isNaN(guess)) {
+    msg = 'Not a valid number';
+    return false;
+  }
+  if (guess < 1 || guess > 100) {
+    msg = 'Not in range of 1 - 100';
+    return false;
+  }
+  return true;
 }
 
 function checkGuess(guess) {
+
+  if (attempts === 0) return;
+
+  attempts--;
+  prevGuess.push(guess);
+  form.reset();
+
   if (guess > randoNum) {
-    lowHigh.innerHTML = 'High'
+    update('High');
   } else if (guess < randoNum) {
-    lowHigh.innerHTML = 'Low'
+    update('Low');
   } else if (guess === randoNum) {
-    lowHigh.innerHTML = `You Won. Number is ${randoNum}`
+    update(`You Won. Number is ${randoNum}`);
+    endGame();
   }
+
+  if (attempts === 0) {
+    endGame();
+    console.log('No attempts left');
+  }
+}
+
+function update(lowHighMsg) {
+  showChances.innerHTML = attempts;
+  lowHigh.innerHTML = lowHighMsg;
+  guesses.innerHTML = prevGuess.join(',') || 'None';
+}
+
+function customAlert() {
+  console.log(msg);
+}
+
+
+function endGame() {
+  userInput.setAttribute('disabled', 'true');
+  submitBtn.classList.remove('hover');
+  submitBtn.style.cursor = 'default';
+  submitBtn.setAttribute('disabled', 'true');
 }
