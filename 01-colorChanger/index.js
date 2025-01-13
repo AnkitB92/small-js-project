b3f2db5c201dca3318acd78cf577b5b564f11574
Ankit Bhatt <ankit92b@gmail.com>
2025-01-13 15:51:14 +0530

    Add reset button to revert background color

--- 01-colorChanger/index.js
+++ 01-colorChanger/index.js
@@ -1,9 +1,13 @@
 const buttons = document.querySelectorAll('.btn');
 const main = document.querySelector('main');
-
+const resetBtn = document.querySelector('#reset-btn');
 
 buttons.forEach(btn => {
   btn.addEventListener('click', (e) => {
     main.style.backgroundColor = e.target.id;
   })
 })
+
+resetBtn.onclick = function () {
+  main.style.backgroundColor = '';
+}
