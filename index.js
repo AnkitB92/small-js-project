346d3a77ada24c1462e82f14e3c486a05be7afab
Ankit Bhatt <ankit92b@gmail.com>
2025-02-19 18:41:24 +0530

    Add logic for rendering project list

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-console.log('running...')
+
+
+export function addNav(active) {
+
+  const menu = document.querySelector('.menu');
+
+  if (!menu) return;
+
+  const items = [
+    {
+      name: 'Color Changer',
+      href: '/01-colorchanger/index.html'
+    },
+    {
+      name: 'BMI Calculator',
+      href: '/02-bmicalculator/index.html'
+    },
+    {
+      name: 'Digital Clock',
+      href: '/03-digitalClock/index.html'
+    },
+    {
+      name: 'Guess The Number',
+      href: '/04-guessTheNumber/index.html'
+    },
+    {
+      name: 'Random Color Generator',
+      href: '/05-randomColorGenerator/index.html'
+    },
+    {
+      name: 'Display Keys',
+      href: '/06_displayKey/index.html'
+    },
+  ]
+
+  const menuList = document.createElement('div');
+  menuList.classList.add('menu-list');
+
+  items.map(item => {
+    const link = document.createElement('a');
+    link.href = item.href;
+    link.textContent = item.name;
+
+    active === item.name && link.classList.add('active');
+    menuList.appendChild(link);
+  })
+
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  checkBox.id = 'menu-check';
+
+  menu.append(checkBox, menuList);
+}
