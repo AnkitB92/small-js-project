60385ceae660f7c7e11b84b6d0c1940090207991
Ankit Bhatt <ankit92b@gmail.com>
2025-04-11 13:22:12 +0530

    Update href path

--- index.js
+++ index.js
@@ -9,27 +9,27 @@ export function addNav(active) {
   const items = [
     {
       name: 'Color Changer',
-      href: './01-colorchanger/'
+      href: '/01-colorchanger/'
     },
     {
       name: 'BMI Calculator',
-      href: './02-bmicalculator/'
+      href: '/02-bmicalculator/'
     },
     {
       name: 'Digital Clock',
-      href: './03-digitalClock/'
+      href: '/03-digitalClock/'
     },
     {
       name: 'Guess The Number',
-      href: './04-guessTheNumber/'
+      href: '/04-guessTheNumber/'
     },
     {
       name: 'Random Color Generator',
-      href: './05-randomColorGenerator/'
+      href: '/05-randomColorGenerator/'
     },
     {
       name: 'Display Keys',
-      href: './06_displayKey/'
+      href: '/06_displayKey/'
     },
   ]
 
